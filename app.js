a1e033bf99ead85bffe91a0b6f0324f0803fcad0
joao pedro rossatti dal agnol <jp.dalagnol@gmail.com>
2025-06-11 17:24:07 -0300

    implementar a funcao sortear

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-teste
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+    
+    for(i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+
+    alert(sorteados);
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
